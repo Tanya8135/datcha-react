f0253dc747c3c253193ced454803e71c600c00b0
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2024-08-18 10:07:51 +0300

    add test photo

--- src/components/Photo/PhotoCommon/PhotoCommon.jsx
+++ src/components/Photo/PhotoCommon/PhotoCommon.jsx
@@ -5,10 +5,35 @@ function PhotoCommon() {
     <div>
       <h4 className={css.subtitlePhoto}>Загальні</h4>
 
-      <img
-        src={process.env.PUBLIC_URL + '/images/1180/altanka1_700.jpg'}
-        alt="Альтанка"
-      />
+      <div className={css.testCommon}>
+        <div className={css.test1}>
+          <img
+            srcSet={`${process.env.PUBLIC_URL}/images/1180/altanka1@2_700.jpg 1400w,
+      ${process.env.PUBLIC_URL}/images/1180/altanka1_700.jpg 700w`}
+            sizes="(min-width: 1180px) 700px, 100vw"
+            src={process.env.PUBLIC_URL + '/images/1180/altanka1_700.jpg'}
+            alt="Альтанка"
+          />
+        </div>
+        <div className={css.test1}>
+          <img
+            srcSet={`${process.env.PUBLIC_URL}/images/1180/altanka2@2_700.jpg 1400w,
+      ${process.env.PUBLIC_URL}/images/1180/altanka2_700.jpg 700w`}
+            sizes="(min-width: 1180px) 700px, 100vw"
+            src={process.env.PUBLIC_URL + '/images/1180/altanka2_700.jpg'}
+            alt="Альтанка"
+          />
+        </div>
+        <div className={css.test1}>
+          <img
+            srcSet={`${process.env.PUBLIC_URL}/images/1180/altanka3@2_700.jpg 1400w,
+      ${process.env.PUBLIC_URL}/images/1180/altanka3_700.jpg 700w`}
+            sizes="(min-width: 1180px) 700px, 100vw"
+            src={process.env.PUBLIC_URL + '/images/1180/altanka3_700.jpg'}
+            alt="Альтанка"
+          />
+        </div>
+      </div>
     </div>
   );
 }
