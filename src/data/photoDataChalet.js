15b95f8bd5dd475fd9b5c4d4d15f5bc2dc503e9d
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2024-08-30 12:05:27 +0300

    refactoring photo fot PhotoChalet

--- src/data/photoDataChalet.js
+++ src/data/photoDataChalet.js
@@ -1,5 +1,13 @@
 const photoDataChalet = [
     { id: 1, src: `${process.env.PUBLIC_URL}/images/chaletPhoto/chalet.jpg`, alt: 'Шале' },
+    { id: 2, src: `${process.env.PUBLIC_URL}/images/chaletPhoto/chalet1.jpg`, alt: 'Шале' },
+    { id: 3, src: `${process.env.PUBLIC_URL}/images/chaletPhoto/ladder_chalet1.jpg`, alt: 'Сходи, шале, перший поверх' },
+    { id: 4, src: `${process.env.PUBLIC_URL}/images/chaletPhoto/first_floor.jpg`, alt: 'Перший поверх' },
+    { id: 5, src: `${process.env.PUBLIC_URL}/images/chaletPhoto/kitchen_chalet.jpg`, alt: 'Кухня' },
+    { id: 6, src: `${process.env.PUBLIC_URL}/images/chaletPhoto/second_floor.jpg`, alt: 'Другий поверх, шале' },
+    { id: 7, src: `${process.env.PUBLIC_URL}/images/chaletPhoto/second_floor_bed.jpg`, alt: 'Другий поверх, ліжко, шале' },
+
+
 ]
 
 export default photoDataChalet;
