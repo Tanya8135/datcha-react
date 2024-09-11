4e8fcc5d6d0365fe5111df4e7edc321c8ce03777
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2024-09-11 15:11:05 +0300

    add style theme for burger-menu

--- src/components/Header/BurgerMenu/BurgerMenu.jsx
+++ src/components/Header/BurgerMenu/BurgerMenu.jsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectTheme } from '../../../redux/selectors';
 
 // import css from "./BurgerMenu.module.css";
 import css from './BurgerMenu.module.scss';
-import BtnClose from "./BtnClose";
-import BurgerMenuContent from "./BurgerMenuContent";
+import BtnClose from './BtnClose';
+import BurgerMenuContent from './BurgerMenuContent';
 
 function BurgerMenu({ isMenuOpen, toggleMenu }) {
+  const darkTheme = useSelector(selectTheme);
+
   return (
     <div
       className={`${css.menuContainer} ${
-        isMenuOpen ? css.jsMenuContainer : ""
+        isMenuOpen ? css.jsMenuContainer : ''
       }`}
+      style={{ background: darkTheme === 'light' ? '#fffbdd' : '#5b594b' }}
     >
       {/* btn-function */}
       {isMenuOpen && (
