11ce6ea4853a617d36b9477377b9ae520d6edf0f
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2024-09-15 19:13:19 +0300

    chaged photo for component PhotoChalet

--- src/data/photoDataChalet.js
+++ src/data/photoDataChalet.js
@@ -6,28 +6,90 @@ const photoDataChalet = [
         ${process.env.PUBLIC_URL}/images/photo/240/chalet.jpg 240w,
         ${process.env.PUBLIC_URL}/images/photo/341/chalet.jpg 341w,
         ${process.env.PUBLIC_URL}/images/photo/376/chalet.jpg 376w,
-        ${process.env.PUBLIC_URL}/images/photo/240/chalet@2.jpg 240w,
-        ${process.env.PUBLIC_URL}/images/photo/341/chalet@2.jpg 341w,
-        ${process.env.PUBLIC_URL}/images/photo/376/chalet@2.jpg 376w
+        ${process.env.PUBLIC_URL}/images/photo/240/chalet@2.jpg 480w,
+        ${process.env.PUBLIC_URL}/images/photo/341/chalet@2.jpg 682w,
+        ${process.env.PUBLIC_URL}/images/photo/376/chalet@2.jpg 572w
         `,
         alt: 'Шале'
     },
-    { id: 2, 
-        src: `${process.env.PUBLIC_URL}/images/photo/240/chalet1.jpg`, 
+    {
+        id: 2,
+        src: `${process.env.PUBLIC_URL}/images/photo/240/chalet1.jpg`,
         srcSet: `
         ${process.env.PUBLIC_URL}/images/photo/240/chalet1.jpg 240w,
         ${process.env.PUBLIC_URL}/images/photo/341/chalet1.jpg 341w,
         ${process.env.PUBLIC_URL}/images/photo/376/chalet1.jpg 376w,
-        ${process.env.PUBLIC_URL}/images/photo/240/chalet1@2.jpg 240w,
-        ${process.env.PUBLIC_URL}/images/photo/341/chalet1@2.jpg 341w,
-        ${process.env.PUBLIC_URL}/images/photo/376/chalet1@2.jpg 376w
+        ${process.env.PUBLIC_URL}/images/photo/240/chalet1@2.jpg 480w,
+        ${process.env.PUBLIC_URL}/images/photo/341/chalet1@2.jpg 682w,
+        ${process.env.PUBLIC_URL}/images/photo/376/chalet1@2.jpg 572w
         `,
-        alt: 'Шале' },
-    { id: 3, src: `${process.env.PUBLIC_URL}/images/chaletPhoto/ladder_chalet1.jpg`, alt: 'Сходи, шале, перший поверх' },
-    { id: 4, src: `${process.env.PUBLIC_URL}/images/chaletPhoto/first_floor.jpg`, alt: 'Перший поверх' },
-    { id: 5, src: `${process.env.PUBLIC_URL}/images/chaletPhoto/kitchen_chalet.jpg`, alt: 'Кухня' },
-    { id: 6, src: `${process.env.PUBLIC_URL}/images/chaletPhoto/second_floor.jpg`, alt: 'Другий поверх, шале' },
-    { id: 7, src: `${process.env.PUBLIC_URL}/images/chaletPhoto/second_floor_bed.jpg`, alt: 'Другий поверх, ліжко, шале' },
+        alt: 'Шале'
+    },
+    {
+        id: 3,
+        src: `${process.env.PUBLIC_URL}/images/photo/240/lodder_chalet.jpg`,
+        srcSet: `
+        ${process.env.PUBLIC_URL}/images/photo/240/lodder_chalet.jpg 240w,
+        ${process.env.PUBLIC_URL}/images/photo/341/lodder_chalet.jpg 341w,
+        ${process.env.PUBLIC_URL}/images/photo/376/lodder_chalet.jpg 376w,
+        ${process.env.PUBLIC_URL}/images/photo/240/lodder_chalet@2.jpg 480w,
+        ${process.env.PUBLIC_URL}/images/photo/341/lodder_chalet@2.jpg 682w,
+        ${process.env.PUBLIC_URL}/images/photo/376/lodder_chalet@2.jpg 572w
+        `,
+        alt: 'Сходи, шале, перший поверх'
+    },
+    {
+        id: 4,
+        src: `${process.env.PUBLIC_URL}/images/photo/240/first_floor.jpg`,
+        srcSet: `
+        ${process.env.PUBLIC_URL}/images/photo/240/first_floor.jpg 240w,
+        ${process.env.PUBLIC_URL}/images/photo/341/first_floor.jpg 341w,
+        ${process.env.PUBLIC_URL}/images/photo/376/first_floor.jpg 376w,
+        ${process.env.PUBLIC_URL}/images/photo/240/first_floor@2.jpg 480w,
+        ${process.env.PUBLIC_URL}/images/photo/341/first_floor@2.jpg 682w,
+        ${process.env.PUBLIC_URL}/images/photo/376/first_floor@2.jpg 572w
+        `,
+        alt: 'Перший поверх'
+    },
+    {
+        id: 5,
+        src: `${process.env.PUBLIC_URL}/images/photo/240/kitchen_chalet.jpg`,
+        srcSet: `
+        ${process.env.PUBLIC_URL}/images/photo/240/kitchen_chalet.jpg 240w,
+        ${process.env.PUBLIC_URL}/images/photo/341/kitchen_chalet.jpg 341w,
+        ${process.env.PUBLIC_URL}/images/photo/376/kitchen_chalet.jpg 376w,
+        ${process.env.PUBLIC_URL}/images/photo/240/kitchen_chalet@2.jpg 480w,
+        ${process.env.PUBLIC_URL}/images/photo/341/kitchen_chalet@2.jpg 682w,
+        ${process.env.PUBLIC_URL}/images/photo/376/kitchen_chalet@2.jpg 572w
+        `,
+        alt: 'Кухня'
+    },
+    {
+        id: 6,
+        src: `${process.env.PUBLIC_URL}/images/photo/240/second_floor.jpg`,
+        srcSet: `
+        ${process.env.PUBLIC_URL}/images/photo/240/second_floor.jpg 240w,
+        ${process.env.PUBLIC_URL}/images/photo/341/second_floor.jpg 341w,
+        ${process.env.PUBLIC_URL}/images/photo/376/second_floor.jpg 376w,
+        ${process.env.PUBLIC_URL}/images/photo/240/second_floor@2.jpg 480w,
+        ${process.env.PUBLIC_URL}/images/photo/341/second_floor@2.jpg 682w,
+        ${process.env.PUBLIC_URL}/images/photo/376/second_floor@2.jpg 572w
+        `,
+        alt: 'Другий поверх, шале'
+    },
+    {
+        id: 7,
+        src: `${process.env.PUBLIC_URL}/images/photo/240/second_floor_bed.jpg`,
+        srcSet: `
+        ${process.env.PUBLIC_URL}/images/photo/240/second_floor_bed.jpg 240w,
+        ${process.env.PUBLIC_URL}/images/photo/341/second_floor_bed.jpg 341w,
+        ${process.env.PUBLIC_URL}/images/photo/376/second_floor_bed.jpg 376w,
+        ${process.env.PUBLIC_URL}/images/photo/240/second_floor_bed@2.jpg 480w,
+        ${process.env.PUBLIC_URL}/images/photo/341/second_floor_bed@2.jpg 682w,
+        ${process.env.PUBLIC_URL}/images/photo/376/second_floor_bed@2.jpg 572w
+        `,
+        alt: 'Другий поверх, ліжко, шале'
+    },
 
 
 ]
