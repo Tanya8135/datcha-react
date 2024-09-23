48fb9461cf900f05a6df91fb1685e9e187798fad
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2024-09-23 13:14:21 +0300

    create toggle language

--- src/components/LanguageToggle/LanguageToggle.jsx
+++ src/components/LanguageToggle/LanguageToggle.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { changeLanguage } from '../../redux/languageSlice';
@@ -8,29 +9,40 @@ import css from './LanguageToggle.module.scss';
 function LanguageToggle() {
   const dispatch = useDispatch();
   const { i18n } = useTranslation();
+  // const { language, isActive } = useSelector(selectLanguage);
   const currentLanguage = useSelector(selectLanguage);
+  const isActive = currentLanguage === 'ru';
 
   const handleLanguageChange = lng => {
     i18n.changeLanguage(lng);
     dispatch(changeLanguage(lng));
   };
 
+  const handleToggleClick = () => {
+    const newLanguage = currentLanguage === 'ua' ? 'ru' : 'ua';
+    handleLanguageChange(newLanguage);
+  };
+
+  useEffect(() => {
+    i18n.changeLanguage(currentLanguage);
+  }, [currentLanguage, i18n]);
+
   return (
     <>
-      <button
-        className={css.btnTest}
-        onClick={() => handleLanguageChange('ru')}
-        disabled={currentLanguage === 'ru'}
-      >
-        рус.
-      </button>
-      <button
-        className={css.btnTest}
-        onClick={() => handleLanguageChange('ua')}
-        disabled={currentLanguage === 'ua'}
-      >
-        Укр.
-      </button>
+      <div className={css.toggleLng} onClick={handleToggleClick}>
+        <div
+          className={`${css.circleLng} ${isActive ? css.activeR : css.activeL}`}
+        ></div>
+
+        <ul className={css.listLng}>
+          <li className={css.itemLng}>
+            <p className={css.textLng}>UA</p>
+          </li>
+          <li className={css.itemLng}>
+            <p className={css.textLng}>RU</p>
+          </li>
+        </ul>
+      </div>
     </>
   );
 }
