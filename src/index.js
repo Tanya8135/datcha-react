e3f7d584b0d7fea8fa5b49ff4b636e41fab1788c
tanya8135 <108739694+Tanya8135@users.noreply.github.com>
2025-01-04 16:52:23 +0200

    delete content in basename->index.js

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename='/datcha-react'>
+        <BrowserRouter basename=''>
           <I18nextProvider i18n={i18n}>
             <App />
           </I18nextProvider>
